dca8abed5de4340e0269762654bdb30342e24968
pkuklin <kuklinpavlo@gmail.com>
2017-06-22 17:16:18 +0300

    progress - lecture 33

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 import App from './components/app';
 import reducers from './reducers';
 
